131660807ec82ae94dbf72efd3e048cc806e1995
milankatira <milankatira26@gmail.com>
2023-06-07 17:27:25 +0530

    update config

--- src/config/db.js
+++ src/config/db.js
@@ -2,8 +2,9 @@ const mongoose = require('mongoose');
 
 exports.db = async () => {
   try {
+    mongoose.set('strictQuery', false);
     console.log('Mongodb connected successfully.', process.env.MONGODB_URI);
-    await mongoose.connect(process.env.MONGODB_URI);
+    await mongoose.connect(process.env.MONGODB_URI,{ useNewUrlParser: true, useUnifiedTopology: true });
   } catch (error) {
     console.log(process.env.MONGODB_URI);
     console.log('Database connection error: ', error);
